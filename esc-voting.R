429a5c89368f8119403b6a3ca028ce1e633a60a9
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-05-17 12:41:07 +0200

    load and prepare dataset

--- esc-voting.R
+++ esc-voting.R
@@ -14,10 +14,31 @@ setwd(wd)
 
 
 ## load data from disk
-## if doesn't exist, pull data from DWD
-if(file.exists("dwd.Rdata")){
-  load("dwd.Rdata")
-} else {
-  dta <- f.get.dwd.data()
-  save(dta, file = "dwd.Rdata")
-}
+# ## if doesn't exist, pull data from DWD
+# if(file.exists("dwd.Rdata")){
+#   load("dwd.Rdata")
+# } else {
+#   dta <- f.get.dwd.data()
+#   save(dta, file = "dwd.Rdata")
+# }
+
+# drops the televote points column for some reason
+# library(readxl)
+#esc.2016 <- read_excel("data/ESC-2016-grand_final-full_results.xls", skip = 1)
+
+# convert to csv first, then clean \n characters (I hate Excel...)
+esc.2016 <- read_csv2("data/ESC-2016-grand_final-full_results.csv") %>% 
+  # rename column names to clean R standards
+  setNames(make.names(names(.))) %>% 
+  mutate(
+    # clean up NA to zeros
+    Jury.Points = ifelse(is.na(Jury.Points), 0, Jury.Points),
+    Televote.Points = ifelse(is.na(Televote.Points), 0, Televote.Points),
+    # get total points as sum of Jury and Televote
+    Total.Points = Jury.Points + Televote.Points
+  )
+
+
+
+
+
